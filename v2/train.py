dcb84f99664b5780ea1be6bf0c5dc6de63e0dc8d
JMcGuireCE <143644210+JMcGuireCE@users.noreply.github.com>
2025-08-06 14:57:08 -0400

    New training method
    Train with modern Keras. Now the model sees the entire dataset every
    epoch.

---
+++ v2/train.py
@@ -0,0 +1,29 @@
+# Copyright (c) UofSC ARTS Lab, 2025
+# Signal Compensation LSTM model training
+
+import keras
+import numpy as np
+from training_utils import WindowGenerator
+
+input_data = np.load('./../dataset/benchtop_test_v6/Test 2/package.npy').reshape(1, -1, 1)
+output_data = np.load('./../dataset/benchtop_test_v6/Test 2/reference.npy').reshape(1, -1, 1)
+
+windows = WindowGenerator(input_data, output_data, window_size=1200)
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(None,1)),
+    keras.layers.LSTM(units=50),
+    keras.layers.Dense(units=1)
+    ])
+
+model.summary()
+model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=0.001),
+        loss=keras.losses.mean_squared_error
+        )
+
+model.fit(
+        windows,
+        shuffle=True,
+        epochs=20
+        )
---
+++ v2/training_utils.py
@@ -0,0 +1,26 @@
+# Copyright (c) UofSC ARTS Lab, 2025
+# Classes and tools used for training the model
+
+import keras
+import numpy as np
+from math import floor, ceil
+from random import shuffle
+
+# Splits the dataset into a series of windows of a given size
+class WindowGenerator(keras.utils.PyDataset):
+    def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int=1200):
+        self.window_size = window_size
+        self.x = x
+        self.y = y
+        print(len(self.x))
+        print(self.x)
+
+
+    def __len__(self):
+        print(ceil(len(self.x) / self.window_size))
+        return ceil(self.x.size / self.window_size)
+
+
+    def __getitem__(self, index):
+        return (self.x[index * self.window_size: index * self.window_size + self.window_size],
+                self.y[index * self.window_size: index * self.window_size + self.window_size])
