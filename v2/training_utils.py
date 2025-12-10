7cad8fb8319e0a80102d10177c5d0e194a03b732
JMcGuireCE <143644210+JMcGuireCE@users.noreply.github.com>
2025-12-09 22:33:29 -0500

    Fix dimension indexing in window generator
    The window generator was selecting the right window along the wrong
    axis, causing issues with training.

--- v2/training_utils.py
+++ v2/training_utils.py
@@ -7,7 +7,7 @@ from math import floor, ceil
 from random import shuffle
 
 # Splits the dataset into a series of windows of a given size
-class WindowGenerator(keras.utils.PyDataset):
+class WindowGenerator(keras.utils.Sequence):
     def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int=1200, **kwargs):
         super().__init__(**kwargs)
         self.window_size = window_size
@@ -16,9 +16,9 @@ class WindowGenerator(keras.utils.PyDataset):
 
 
     def __len__(self):
-        return ceil(self.x.size / self.window_size)
+        return floor(self.x.size / self.window_size)
 
 
     def __getitem__(self, index):
-        return (self.x[index * self.window_size: index * self.window_size + self.window_size],
-                self.y[index * self.window_size: index * self.window_size + self.window_size])
+        return (self.x[:,index * self.window_size: index * self.window_size + self.window_size,:],
+                self.y[:,index * self.window_size: index * self.window_size + self.window_size,:])
