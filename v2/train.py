5db95c3fdf46dc4b95ecfa42ca7cde4b9e7a1d1f
JMcGuireCE <143644210+JMcGuireCE@users.noreply.github.com>
2025-12-09 23:43:11 -0500

    Use Butterworth filter
    This will be cleaned up a bit more in the future.

--- v2/train.py
+++ v2/train.py
@@ -3,16 +3,25 @@
 
 import keras
 import numpy as np
+import scipy.signal as signal
 from training_utils import WindowGenerator
 
-input_data = np.load('./../dataset/benchtop_test_v6/Test 2/package.npy').reshape(1, -1, 1)
+input_data = np.load('./../dataset/benchtop_test_v6/Test 2/package.npy')
+filter_b, filter_a = signal.butter(N=1, Wn=10, btype='highpass', fs=400)
+
+input_filtered = signal.filtfilt(filter_b, filter_a, input_data).reshape(1, -1, 1)
 output_data = np.load('./../dataset/benchtop_test_v6/Test 2/reference.npy').reshape(1, -1, 1)
 
-windows = WindowGenerator(input_data, output_data, window_size=1200)
+windows = WindowGenerator(input_filtered, output_data, window_size=1200)
 
 model = keras.Sequential([
     keras.layers.Input(shape=(None,1)),
-    keras.layers.LSTM(units=50),
+
+    keras.layers.LSTM(units=50,
+                      stateful = False,
+                      return_sequences=True
+                      ),
+
     keras.layers.Dense(units=1)
     ])
 
@@ -25,7 +34,7 @@ model.compile(
 model.fit(
         windows,
         shuffle=True,
-        epochs=3
+        epochs=100
         )
 
 model.save('./model.keras')
